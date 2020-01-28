bc1ac358136b19e792e594d330a220286938b50c
alfcolon <alfcolon@gmail.com>
2020-01-28 13:16:07 -0800

    Uploading project files

---
+++ BasicAnimations/BasicAnimations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BasicAnimations
+//
+//  Created by alfredo on 1/28/20.
+//  Copyright © 2020 Alfredo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
